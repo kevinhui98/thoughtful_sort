7ca0068d05deea1c6d3a2a8f5d41ee3da7e28bae
Kevin Hui <kevinhui98@gmail.com>
2024-10-15 14:06:53 -0400

    adding thought ai sorting

---
+++ thoughtful_sort.py
@@ -0,0 +1,17 @@
+def sort(width, height,length,mass):
+	# width, height, length will have to be in cm
+	# mass is kg
+	dimensions = (width * height)* length
+	size = "clear"
+	if width == 150 or height == 150 or length == 150 or dimensions >= 1000000:
+		size = "bulky"
+	if size == "clear" and mass < 20:
+		return "STANDARD"
+	elif size =="bulky" and mass >= 20:
+		return "REJECTED"
+	else:
+		return "SPECIAL"
+print(sort(100,100,230,19))
+print(sort(100,100,23,19))
+print(sort(100,100,23,20))
+print(sort(100,100,230,20))
